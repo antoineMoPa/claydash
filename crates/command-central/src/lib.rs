1a95e43cb0d7949866239e7998a968b459b6485d
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2023-10-27 01:37:09 -0400

    Bug fix

--- crates/command-central/src/lib.rs
+++ crates/command-central/src/lib.rs
@@ -14,7 +14,6 @@
 use std::collections::HashMap;
 use lazy_static::lazy_static;
 use std::sync::Mutex;
-use std::sync::MutexGuard;
 
 #[derive(Clone)]
 pub struct Command {
@@ -30,9 +29,6 @@ impl Command {
         self.requested_runs = self.requested_runs + 1;
     }
 
-    /// Returns true and decrements internal counter if the command has to be run.
-    ///
-    /// Returns false if nothing has to be done.
     fn check_if_has_to_run(&mut self) -> bool {
         if self.requested_runs > 0 {
             self.requested_runs = self.requested_runs - 1;
@@ -57,21 +53,37 @@ lazy_static! {
     static ref COMMANDS_MAP: Mutex<HashMap<String, Command>> = Mutex::new(HashMap::new());
 }
 
-pub fn add_command(system_name: String, command: Command) {
+pub fn add_command(system_name: &String, command: Command) {
     lazy_static::initialize(&COMMANDS_MAP);
     let mut commands = COMMANDS_MAP.lock().unwrap();
 
-    if commands.contains_key(&system_name) {
+    if commands.contains_key(system_name) {
         panic!("Command {} already defined.", system_name);
     }
 
-    commands.insert(system_name, command);
+    commands.insert(system_name.clone(), command);
 }
 
 /// Returns a copy of the command
-pub fn read_command(system_name: String) -> Option<Command> {
+pub fn read_command(system_name: &String) -> Option<Command> {
     let commands = COMMANDS_MAP.lock().unwrap();
-    return commands.get(&system_name).cloned();
+    return commands.get(system_name).cloned();
+}
+
+/// Returns true and decrements internal counter if the command has to be run.
+///
+/// Returns false if nothing has to be done.
+pub fn check_if_has_to_run(system_name: &String) -> bool {
+    let mut commands = COMMANDS_MAP.lock().unwrap();
+    let command = commands.get_mut(system_name);
+    return command.unwrap().check_if_has_to_run();
+}
+
+/// Requests to run a command by name.
+pub fn request_run(system_name: &String) {
+    let mut commands = COMMANDS_MAP.lock().unwrap();
+    let command = commands.get_mut(system_name);
+    return command.unwrap().run();
 }
 
 
@@ -81,12 +93,12 @@ mod tests {
 
     #[test]
     fn it_adds_and_gets_new_command() {
-        add_command("test-command".to_string(), Command {
+        add_command(&"test-command".to_string(), Command {
             title: "Test Command".to_string(),
             docs: "Here are some docs about the command".to_string(),
             ..Command::default()
         });
-        let command = read_command("test-command".to_string()).unwrap();
+        let command = read_command(&"test-command".to_string()).unwrap();
         assert_eq!(command.title, "Test Command".to_string());
     }
 
@@ -96,7 +108,7 @@ mod tests {
     #[test]
     #[should_panic]
     fn handles_not_found_commands() {
-        read_command("not-existing-command".to_string()).unwrap();
+        read_command(&"not-existing-command".to_string()).unwrap();
     }
 
     // This test is useful, but it causes other tests to panic.
@@ -105,12 +117,12 @@ mod tests {
     #[test]
     #[should_panic]
     fn it_detects_if_command_already_exists() {
-        add_command("test-command-duplicated".to_string(), Command {
+        add_command(&"test-command-duplicated".to_string(), Command {
             title: "Test Command".to_string(),
             docs: "Here are some docs about the command".to_string(),
             ..Command::default()
         });
-        add_command("test-command-duplicated".to_string(), Command {
+        add_command(&"test-command-duplicated".to_string(), Command {
             title: "Test Command".to_string(),
             docs: "Here are some docs about the command".to_string(),
             ..Command::default()
@@ -119,25 +131,26 @@ mod tests {
 
     #[test]
     fn runs_commands() {
-        add_command("test-command-with-callback".to_string(), Command {
+        let sys_name = "test-command-with-callback".to_string();
+        add_command(&sys_name, Command {
             title: "Test Command".to_string(),
             docs: "Here are some docs about the command".to_string(),
             ..Command::default()
         });
-        let mut command = read_command("test-command-with-callback".to_string()).unwrap();
+        read_command(&sys_name).unwrap();
 
-        command.run();
+        request_run(&sys_name);
 
         // Should return true 1 time
-        assert_eq!(command.check_if_has_to_run(), true);
-        assert_eq!(command.check_if_has_to_run(), false);
+        assert_eq!(check_if_has_to_run(&sys_name), true);
+        assert_eq!(check_if_has_to_run(&sys_name), false);
 
-        command.run();
-        command.run();
+        request_run(&sys_name);
+        request_run(&sys_name);
 
         // Should return true 2 times
-        assert_eq!(command.check_if_has_to_run(), true);
-        assert_eq!(command.check_if_has_to_run(), true);
-        assert_eq!(command.check_if_has_to_run(), false);
+        assert_eq!(check_if_has_to_run(&sys_name), true);
+        assert_eq!(check_if_has_to_run(&sys_name), true);
+        assert_eq!(check_if_has_to_run(&sys_name), false);
     }
 }
