98428cb195c48dd03e394723bdd1c59d9c05eca4
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2023-10-30 12:01:13 -0400

    Use storage buggers instead of uniforms.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // This is only for native builds
 #[allow(unused_imports)]
 use std::fs::read_to_string;
-use command_central::Command;
+use command_central::CommandInfo;
 
 use bevy_reflect::{
     TypePath,
@@ -42,19 +42,18 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(bevy_framepace::FramepacePlugin)
         .add_plugins(DefaultPickingPlugins)
-        .add_plugins(MaterialPlugin::<CustomMaterial>::default())
+        .add_plugins(MaterialPlugin::<SDFObjectMaterial>::default())
         .add_plugins((
             FrameTimeDiagnosticsPlugin,
             LogDiagnosticsPlugin::default()
         ))
         .add_systems(Startup, remove_picking_logs)
-        .add_systems(Startup, setup_commands)
+        .add_systems(Startup, register_commands)
         .add_systems(Startup, setup_frame_limit)
         .add_systems(Startup, setup_camera)
         .add_systems(Startup, setup_window_size)
         .add_systems(Startup, build_projection_surface)
         .add_systems(Update, keyboard_input_system)
-        .add_systems(Update, launch_mouse_commands)
         .add_systems(Update, run_commands)
         .run();
 }
@@ -63,24 +62,6 @@ fn remove_picking_logs (
     mut logging_next_state: ResMut<NextState<debug::DebugPickingMode>>,
 ) {
     logging_next_state.set(debug::DebugPickingMode::Disabled);
-}
-
-fn setup_commands() {
-    let mut params = command_central::CommandMap::new();
-    params.insert("x".to_string(), command_central::CommandParam {
-        docs: "X position of the mouse.".to_string(),
-        ..default()
-    });
-    params.insert("y".to_string(), command_central::CommandParam {
-        docs: "Y position of the mouse.".to_string(),
-        ..default()
-    });
-
-    command_central::add_command(&"test-command".to_string(), Command {
-        title: "Test Command".to_string(),
-        docs: "Here are some docs about the command".to_string(),
-        ..Command::default()
-    });
 }
 
 fn setup_frame_limit(mut settings: ResMut<bevy_framepace::FramepaceSettings>) {
@@ -116,27 +97,43 @@ fn keyboard_input_system(
     }
 }
 
-const MAX_SDFS_PER_ENTITY: usize = 512;
+const MAX_SDFS_PER_ENTITY: i32 = 512;
 
-#[derive(TypeUuid, TypePath, AsBindGroup, Debug, Clone)]
+#[derive(TypeUuid, TypePath, AsBindGroup, Clone)]
 #[uuid = "84F24BEA-CC34-4A35-B223-C5C148A14722"]
-struct CustomMaterial {
+#[repr(C,align(16))]
+struct SDFObjectMaterial {
     #[uniform(0)]
-    objects: [IVec4; MAX_SDFS_PER_ENTITY],
-    #[uniform(1)]
-    mouse: Vec3,
+    mouse: Vec4,
+    #[storage(1, read_only)]
+    sdf_types: Vec<i32>,
+    #[storage(2, read_only)]
+    sdf_positions: Vec<Vec4>, // Vec4 is 16 bit aligned. It makes our life easier than Vec3
 }
 
-impl Default for CustomMaterial {
+
+// SDF types definition
+const TYPE_END: i32 = 0; // No more SDF to process
+const TYPE_SPHERE: i32 = 1;
+const TYPE_RECTANGLE: i32 = 2;
+
+impl Default for SDFObjectMaterial {
     fn default() -> Self {
-        Self {
-            objects: [IVec4 { w: 0, x: 1, y: 2, z: 3}; MAX_SDFS_PER_ENTITY],
-            mouse: Vec3 { x: 0.0, y: 0.0, z: 0.0 },
-        }
+        let mut default = Self {
+            sdf_types: Vec::new(),
+            sdf_positions: Vec::new(),
+            mouse: Vec4 { x: 0.0, y: 0.0, z: 0.0, w: 0.0 },
+        };
+
+        default.sdf_types.push(TYPE_END);
+        default.sdf_positions.push(Vec4::new(0.0, 0.0, 0.0, 0.0));
+
+        return default;
     }
 }
 
-impl Material for CustomMaterial {
+
+impl Material for SDFObjectMaterial {
     fn fragment_shader() -> ShaderRef {
         return "shaders/all.wgsl".into();
     }
@@ -148,8 +145,18 @@ impl Material for CustomMaterial {
         key: MaterialPipelineKey<Self>,
     ) -> Result<(), SpecializedMeshPipelineError> {
         let fragment = descriptor.fragment.as_mut().unwrap();
-        let val = ShaderDefVal::UInt("MAX_SDFS_PER_ENTITY".into(), MAX_SDFS_PER_ENTITY as u32);
-        fragment.shader_defs.push(val);
+        ShaderDefVal::Int("MAX_SDFS_PER_ENTITY".into(), MAX_SDFS_PER_ENTITY);
+
+        let defs = &mut fragment.shader_defs;
+
+        defs.push(ShaderDefVal::Int(
+            "MAX_SDFS_PER_ENTITY".into(),
+            MAX_SDFS_PER_ENTITY)
+        );
+        defs.push(ShaderDefVal::Int("TYPE_END".into(), TYPE_END));
+        defs.push(ShaderDefVal::Int("TYPE_SPHERE".into(), TYPE_SPHERE));
+        defs.push(ShaderDefVal::Int("TYPE_RECTANGLE".into(), TYPE_RECTANGLE));
+
         Ok(())
     }
 }
@@ -170,8 +177,8 @@ use bevy_mod_picking::backend::HitData;
 
 fn on_pointer_move(
     event: Listener<Pointer<Move>>,
-    material_handle: Query<&Handle<CustomMaterial>>,
-    mut materials: ResMut<Assets<CustomMaterial>>,
+    material_handle: Query<&Handle<SDFObjectMaterial>>,
+    mut materials: ResMut<Assets<SDFObjectMaterial>>,
 ) {
     let hit: &HitData = &event.hit;
     let position = match hit.position {
@@ -191,7 +198,7 @@ fn build_projection_surface(
     mut windows: Query<&mut Window>,
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<CustomMaterial>>,
+    mut materials: ResMut<Assets<SDFObjectMaterial>>,
 ) {
     let window = windows.single_mut();
     let window_aspect_ratio = (window.resolution.physical_width() as f32) / (window.resolution.physical_height() as f32);
@@ -206,46 +213,98 @@ fn build_projection_surface(
                 scale: Vec3::new(1.0, 1.0, window_aspect_ratio),
                 ..default()
             },
-            material: materials.add(CustomMaterial { ..default() }),
+            material: materials.add(SDFObjectMaterial { ..default() }),
             ..default()
         },
         PickableBundle::default(),      // Makes the entity pickable
         RaycastPickTarget::default(),   // Marker for the `bevy_picking_raycast` backend
         On::<Pointer<Move>>::run(on_pointer_move),
+        On::<Pointer<Down>>::run(on_mouse_down),
     ));
 }
 
-fn launch_mouse_commands(
+fn register_commands() {
+    let mut params = command_central::CommandMap::new();
+    params.insert("x".to_string(), command_central::CommandParam {
+        docs: "X position of the sphere.".to_string(),
+        ..default()
+    });
+    params.insert("y".to_string(), command_central::CommandParam {
+        docs: "Y position of the sphere.".to_string(),
+        ..default()
+    });
+    params.insert("z".to_string(), command_central::CommandParam {
+        docs: "Z position of the sphere.".to_string(),
+        ..default()
+    });
+
+    command_central::add_command(&"spawn-sphere".to_string(), CommandInfo {
+        title: "Spawn sphere".to_string(),
+        docs: "Spawn a sphere at the given position in the current object.".to_string(),
+        ..CommandInfo::default()
+    });
+}
+
+fn on_mouse_down(
+    event: Listener<Pointer<Down>>,
     buttons: Res<Input<MouseButton>>,
-    mut windows: Query<&mut Window>,
 ) {
     if buttons.just_pressed(MouseButton::Left) {
-        let window = windows.single_mut();
-        let position = window.cursor_position().unwrap();
+        let hit: &HitData = &event.hit;
+        let position = match hit.position {
+            Some(position) => position,
+            _ => { return; }
+        };
 
         let mut params = command_central::CommandMap::new();
         params.insert("x".to_string(), command_central::CommandParam {
-            docs: "X position of the mouse.".to_string(),
             float: Some(position.x),
             ..default()
         });
         params.insert("y".to_string(), command_central::CommandParam {
-            docs: "Y position of the mouse.".to_string(),
             float: Some(position.y),
             ..default()
         });
+        params.insert("z".to_string(), command_central::CommandParam {
+            float: Some(position.z),
+            ..default()
+        });
 
-        command_central::run_with_params(&"test-command".to_string(), &params);
+        command_central::run_with_params(&"spawn-sphere".to_string(), &params);
     }
 }
 
-fn run_commands() {
-    let command = command_central::check_if_has_to_run(&"test-command".to_string());
+fn run_commands(
+    material_handle: Query<&Handle<SDFObjectMaterial>>,
+    mut materials: ResMut<Assets<SDFObjectMaterial>>,
+) {
+    let command = command_central::check_if_has_to_run(&"spawn-sphere".to_string());
     match command {
         Some(command) => {
             let x = command.parameters.get("x").unwrap().float.unwrap();
             let y = command.parameters.get("y").unwrap().float.unwrap();
-            info!("Running Command! x: {}, y: {}", x, y);
+            let z = command.parameters.get("z").unwrap().float.unwrap();
+
+            let handle = material_handle.single();
+            let material: &mut SDFObjectMaterial = materials.get_mut(handle).unwrap();
+            let mut last_sdf = 0;
+
+            // Find last object
+            for (i, sdf_type) in material.sdf_types.iter().enumerate()  {
+                if sdf_type == &TYPE_END {
+                    last_sdf = i;
+                }
+            }
+
+            material.sdf_types.push(TYPE_END);
+            material.sdf_positions.push(Vec4::new(0.0, 0.0, 0.0, 0.0));
+
+            material.sdf_types[last_sdf] = TYPE_SPHERE;
+            material.sdf_positions[last_sdf].x = x;
+            material.sdf_positions[last_sdf].y = y;
+            material.sdf_positions[last_sdf].z = z;
+
+            info!("Spawning sphere! x: {}, y: {}, z: {}", material.sdf_positions[0].x, material.sdf_positions[0].y, material.sdf_positions[0].z);
         },
         _ => {
             // Nothing to do
